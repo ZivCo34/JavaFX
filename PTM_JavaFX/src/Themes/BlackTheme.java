331727952380377da2d9e492eaa6ebbe9a0de3d2
Ziv Cohen <ziv.cohen.3456@gmail.com>
2018-12-24 13:49:25 +0200

    commit

---
+++ PTM_JavaFX/src/Themes/BlackTheme.java
@@ -0,0 +1,37 @@
+package Themes;
+
+public class BlackTheme implements IDisplayTheme{
+
+
+
+	@Override
+	public String getHorizontal_Pipe() {
+		return "./Resources/Horizontal_Pipe.jpg";
+	}
+
+	@Override
+	public String getVertical_Pipe() {
+		return "./Resources/Vertical_Pipe.jpg";
+	}
+
+	@Override
+	public String getPipe_7() {
+		return "./Resources/Pipe_7.jpg";
+	}
+
+	@Override
+	public String getPipe_J() {
+		return "./Resources/Pipe_J.jpg";
+	}
+
+	@Override
+	public String getPipe_L() {
+		return "./Resources/Pipe_L.jpg";
+	}
+
+	@Override
+	public String getPipe_F() {
+		return "./Resources/Pipe_F.jpg";
+	}
+
+}
