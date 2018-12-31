0574e2544338bea176fce55196deffacbba2757c
eilonv3 <eilonv3@gmail.com>
2018-12-31 11:23:16 +0200

    the only change that has been made here is making the board (and the screen) dynamic and adaptive

--- PTM_JavaFX/src/view/PipeGameDisplayer.java
+++ PTM_JavaFX/src/view/PipeGameDisplayer.java
@@ -9,12 +9,15 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 public class PipeGameDisplayer extends Canvas{
 
 	char[][] pipeGameData;
 	ThemeInterpeter ti;
-
+	static BorderPane root;
 	public void setPipeGameData(char[][] pipeGameData, IDisplayTheme newDT){
 		this.pipeGameData=pipeGameData;
 		this.ti=new ThemeInterpeter(newDT);
@@ -32,16 +35,13 @@ public class PipeGameDisplayer extends Canvas{
 			GraphicsContext gc=getGraphicsContext2D();
 
 			gc.clearRect(0, 0, W, H);
-
 			for(int i=0;i<pipeGameData.length;i++)
 				for(int j=0;j<pipeGameData[i].length;j++){
 					if(pipeGameData[i][j]!=' ')
 						gc.drawImage(ti.getImage(pipeGameData[i][j]), j*w, i*h, w, h);
 				}
-
 		}
 	}
-
 	public void changePipePosition(int i, int j, int timeToSpin){
 		for(int t=0;t<timeToSpin;t++){
 			switch(pipeGameData[i][j]){
@@ -65,12 +65,46 @@ public class PipeGameDisplayer extends Canvas{
 				break;
 			}
 		}
+		//this is where we should call the viewmodel (30/12)
 		redraw();
 	}
 
 	public void changeTheme(IDisplayTheme newDT){
 		this.ti=new ThemeInterpeter(newDT);
 		redraw();
+		}
+	public static void setBackground(BorderPane root) {
+		BackgroundSize bgSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+		root.setBackground(new Background(new BackgroundImage(new Image("file:Resources/BlueTheme/Background.jpg"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, bgSize)));
+	}
+	//this is the part which makes the canvas adaptable, so it's size is dynamic.
+	public void resize(double width, double height) {
+    super.setWidth(width);
+    super.setHeight(height);
+    redraw();
+	}
+	public boolean isResizable() {
+		return true;
+	}
+	public double minHeight(double width) {
+		return 100;
 	}
 
+	public double maxHeight(double width) {
+		return 1200;
+	}
+
+
+	public double prefHeight(double width) {
+		return minHeight(width);
+	}
+
+
+	public double minWidth(double height) {
+		return 0;
+	}
+
+	public double maxWidth(double height) {
+		return 10000;
+	}
 }
